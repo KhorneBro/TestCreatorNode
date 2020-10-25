4c4320ca108d2b52265b259c03a36f5580d3e063
Dmitrii Veselkov <dmitriiveselkov@gmail.com>
2020-10-25 18:22:53 +0600

    meh

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,11 @@ const router = Router();
 const User = require('../models/User');
 const passport = require('passport');
 
+const middlewarePassport = passport.authenticate('jwt', {session: false})
+
 router.get(
     '/allUsers',
-    passport.authenticate('jwt', {session: false}),
+    middlewarePassport,
     async (req, res) => {
         try {
             const allUsers = await User.find();
@@ -16,7 +18,7 @@ router.get(
     });
 
 router.get('/user/:id',
-    passport.authenticate('jwt', {session: false}),
+    middlewarePassport,
     async (req, res) => {
         try {
             const user = await User.findById({id: req.params.id});
@@ -25,5 +27,48 @@ router.get('/user/:id',
             res.status(500).json({message: e})
         }
     });
+
+router.post('/addUser',
+    middlewarePassport,
+    async (req, res) => {
+        try {
+            const user = await new User({
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password,
+                telegram: req.body.telegram,
+                status: req.body.status,
+            }).save()
+            res.status(201).json(user)
+        } catch (e) {
+            res.status(500).json({message: e})
+        }
+    })
+
+router.patch('/updateUser/:id',
+    middlewarePassport,
+    async (req, res) => {
+        try {
+            const updatedUser = await new User.findOneAndUpdate(
+                {_id: req.params.id},
+                {$set: req.body},
+                {new: true}
+            )
+            res.status(200).json(updatedUser)
+        } catch (e) {
+            res.status(500).json({message: e})
+        }
+    })
+
+router.delete('/deleteUser/:id',
+    middlewarePassport,
+    async (req, res) => {
+        try {
+            await User.findByIdAndDelete({_id: req.params.id})
+            res.status(200).json({message: 'Ползователь был удален'})
+        } catch (e) {
+            res.status(500).json({message: e})
+        }
+    })
 
 module.exports = router;
